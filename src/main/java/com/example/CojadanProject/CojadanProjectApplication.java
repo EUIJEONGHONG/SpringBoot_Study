8d254bbe962d9bb8efe573c8ea598b5d7744271c
EUIJEONGHONG <hha1224@gmail.com>
2024-07-31 21:47:37 +0900

    commit dgaza

---
+++ src/main/java/com/example/CojadanProject/CojadanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CojadanProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CojadanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CojadanProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CojadanProject/CojadanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CojadanProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CojadanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
