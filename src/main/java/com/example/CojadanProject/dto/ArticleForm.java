6bf580661ff713adcf2893baba453da69d8c12f3
EUIJEONGHONG <hha1224@gmail.com>
2024-08-07 21:30:52 +0900

    2일차 ~ 3일차

---
+++ src/main/java/com/example/CojadanProject/dto/ArticleForm.java
@@ -0,0 +1,21 @@
+/*폼데이터를 받아올 그릇
+* 앞서 제목, 내용 두개의 데이털르 받기때문에 필드 두개 생성
+* */
+package com.example.CojadanProject.dto;
+
+public class ArticleForm {
+    private String title;
+    private String content;
+
+//    생성자
+    public ArticleForm(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+//    데이터 수신 확인용 문자열 변환 메서드 추가
+
+    @Override
+    public String toString(){
+        return "ArticleForm{" + "title='"+title+'\'' + ", content='"+content+'\''+'}';
+    }
+}
