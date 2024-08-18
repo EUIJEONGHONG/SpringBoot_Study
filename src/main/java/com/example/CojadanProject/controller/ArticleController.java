190a26064c60a1df1eff16581096e30f6444b4ad
EUIJEONGHONG <hha1224@gmail.com>
2024-08-18 17:12:09 +0900

    8장 데이터삭제하기

--- src/main/java/com/example/CojadanProject/controller/ArticleController.java
+++ src/main/java/com/example/CojadanProject/controller/ArticleController.java
@@ -19,6 +19,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -85,5 +86,21 @@ public class ArticleController {
             articleRepository.save(articleEntity);
         }
         return "redirect:/articles/"+articleEntity.getId();
+    }
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, Model model, RedirectAttributes rttr){
+        log.info("삭제 요청이 들어왔습니다!");
+
+        //1.삭제할 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        //2. 삭제하기
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg","삭제되었습니다.");
+        }
+        //3. 리다이렉트하기
+        return "redirect:/articles";
+
     }
 }
